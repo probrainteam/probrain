116a0981826540f6e1efa024d01ee2aee70104e7
sumi.byun <selina2000@naver.com>
2022-01-11 04:22:20 +0900

    Feat : intro section 생성
    section.intro를 추가하고
    title과 card두개 생성
    card:hover 애니메이션 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,61 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
+import './index.scss';
 
 import Layout from 'components/layout';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <section className="intro">
+      <div className="intro__inner">
+        <div className="intro__title">
+          <h3>
+            프로브레인과 함께 성장할 <br />
+            멋진 동료를 찾고 있어요
+          </h3>
+        </div>
+        <div className="intro__content">
+          <div className="card">
+            <div className="card__title">PROBRAIN TMI1</div>
+            <div className="card__subtitle">&#35;PROBRAIN</div>
+            <div className="card__content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              quasi sint quidem maxime atque provident ipsam ab? Facilis sint
+              repellat reiciendis autem architecto. Consequatur unde eius sit,
+              minus consequuntur magnam.
+            </div>
+            <div className="card__subcontent">this is sub content</div>
+          </div>
+          <div className="card">
+            <div className="card__title"> PROBRAIN TMI2</div>
+            <div className="card__subtitle">&#35;2022 &#35;😀</div>
+            <div className="card__content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              quasi sint quidem maxime atque provident ipsam ab? Facilis sint
+              repellat reiciendis autem architecto. Consequatur unde eius sit,
+              minus consequuntur magnam.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Animi quasi sint quidem maxime atque provident
+              ipsam ab? Facilis sin adipisicing elit. Animi quasi sint quidem
+              maxime atque provident ips architecto. Consequatur unde eius sit,
+              minus consequuntur magnam.
+            </div>
+            <div className="card__subcontent">this is sub content</div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    {/* <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
+    <p>Now go build something great.</p> */}
+    {/* <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
       formats={['auto', 'webp', 'avif']}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
-    />
+    /> */}
   </Layout>
 );
 
