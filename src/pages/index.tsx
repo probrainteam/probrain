bf2feeea3d9cfa3b7997652db97f6bb0ac2ab6c2
sumi.byun <selina2000@naver.com>
2022-01-15 00:54:48 +0900

    Feat : Add c1 component, scroll event : show -> up animation
    ![image](https://user-images.githubusercontent.com/49177223/149545055-84d350b5-8d0b-4b17-a562-e110706d421e.png)
    
    따로 전체 화면에 꽉 차게 보이게 구현 x, 안의 내용에 따라 크기가 변함

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
+import React, { useState, useEffect, FunctionComponent, useRef } from 'react';
+// import { StaticImage } from 'gatsby-plugin-image';
 import './index.scss';
 import Layout from 'components/layout';
 import debounce from 'lodash/debounce';
 
-export function useScroll() {
-  const [scrollY, setScrollY] = useState<number>(0);
-
-  const listener = () => {
-    setScrollY(window.pageYOffset);
-  };
-
+const DetectScrollTextComponent: FunctionComponent = () => {
+  const scrollRef = useRef(null);
   const delay = 15;
-
+  const [isShow, setIsShow] = useState(false);
   useEffect(() => {
-    window.addEventListener('scroll', debounce(listener, delay));
-    return () => window.removeEventListener('scroll', listener);
-  });
-
-  return {
-    scrollY,
+    if (!scrollRef.current) return;
+    window.addEventListener('scroll', debounce(yScrollEvent, delay));
+    return () => {
+      window.removeEventListener('scroll', yScrollEvent);
+    };
+  }, [scrollRef.current]);
+  const yScrollEvent = () => {
+    const scroll = scrollRef.current.getBoundingClientRect();
+    // console.log(scroll.top, window.innerHeight);
+    if (scroll.top < window.innerHeight - 150) {
+      setIsShow(true);
+    } else {
+      setIsShow(false);
+    }
   };
-}
-const LogoComponent = () => {
-  const { scrollY } = useScroll();
   return (
-    <section className={`text ${scrollY > 200 ? 'active' : ''}`}>
+    <section className={`text ${isShow ? 'active' : ''}`} ref={scrollRef}>
       <div className="text__inner">
         <div className="text__main">PROBRAIN</div>
         <div className="text__sub">professional brain group</div>
@@ -33,48 +33,141 @@ const LogoComponent = () => {
     </section>
   );
 };
-const IndexPage = () => (
-  <Layout>
-    <section className="intro">
-      <div className="intro__inner">
-        <div className="intro__title">
-          <p className="intro__title--main">
-            프로브레인과 함께 성장할 <br />
-            멋진 동료를 찾고 있어요
-          </p>
-          <p className="intro__title--sub">2022년 PROBRAIN</p>
-        </div>
-        <div className="intro__content">
-          <div className="card">
-            <div className="card__title">PROBRAIN TMI1</div>
-            <div className="card__subtitle">&#35;PROBRAIN</div>
-            <div className="card__content">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-              quasi sint quidem maxime atque provident ipsam ab? Facilis sint
-              repellat reiciendis autem architecto. Consequatur unde eius sit,
-              minus consequuntur magnam.
+const DetectScrollCardComponent: FunctionComponent = ({ children }) => {
+  const scrollRef = useRef(null);
+  const delay = 15;
+  const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    if (!scrollRef.current) return;
+    window.addEventListener('scroll', debounce(yScrollEvent, delay));
+    // window.addEventListener('scroll', yScrollEvent);
+    return () => {
+      window.removeEventListener('scroll', yScrollEvent);
+    };
+  }, [scrollRef.current]);
+  const yScrollEvent = () => {
+    const scroll = scrollRef.current.getBoundingClientRect();
+    // console.log('top', scroll.top);
+    // setHideElement(scroll.top <= -100);
+    // console.log(scroll.top, window.innerHeight);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (scroll.top < window.innerHeight - 150) {
+      //   console.log('show');
+      setIsShow(true);
+    } else {
+      //   console.log('hidden');
+      setIsShow(false);
+    }
+  };
+
+  return (
+    <div className={`c1__card ${isShow ? 'active' : ''}`} ref={scrollRef}>
+      {children}
+    </div>
+  );
+};
+
+const IndexPage = () => {
+  // const { scrollY } = useScroll();
+  return (
+    <Layout>
+      <section className="intro">
+        <div className="intro__inner">
+          <div className="intro__title">
+            <p className="intro__title--main">
+              프로브레인과 함께 성장할 <br />
+              멋진 동료를 찾고 있어요
+            </p>
+            <p className="intro__title--sub">2022년 PROBRAIN</p>
+          </div>
+          <div className="intro__content">
+            <div className="card">
+              <div className="card__title">PROBRAIN TMI1</div>
+              <div className="card__subtitle">&#35;PROBRAIN</div>
+              <div className="card__content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                quasi sint quidem maxime atque provident ipsam ab? Facilis sint
+                repellat reiciendis autem architecto. Consequatur unde eius sit,
+                minus consequuntur magnam.
+              </div>
+            </div>
+            <div className="card">
+              <div className="card__title"> PROBRAIN TMI2</div>
+              <div className="card__subtitle">&#35;2022 &#35;😀</div>
+              <div className="card__content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                quasi sint quidem maxime atque provident ipsam ab? Facilis sint
+                repellat reiciendis autem architecto. Consequatur unde eius sit,
+                minus consequuntur magnam.Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Animi quasi sint quidem maxime atque provident
+                ipsam ab? Facilis sin adipisicing elit. Animi quasi sint quidem
+                maxime atque provident ips architecto. Consequatur unde eius
+                sit, minus consequuntur magnam.
+              </div>
             </div>
           </div>
-          <div className="card">
-            <div className="card__title"> PROBRAIN TMI2</div>
-            <div className="card__subtitle">&#35;2022 &#35;😀</div>
-            <div className="card__content">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-              quasi sint quidem maxime atque provident ipsam ab? Facilis sint
-              repellat reiciendis autem architecto. Consequatur unde eius sit,
-              minus consequuntur magnam.Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Animi quasi sint quidem maxime atque provident
-              ipsam ab? Facilis sin adipisicing elit. Animi quasi sint quidem
-              maxime atque provident ips architecto. Consequatur unde eius sit,
-              minus consequuntur magnam.
-            </div>
+        </div>
+      </section>
+
+      <DetectScrollTextComponent></DetectScrollTextComponent>
+      <section className="c1">
+        <div className="c1__inner">
+          <div className="c1__content">
+            <DetectScrollCardComponent>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+              laudantium in debitis nisi laboriosam possimus saepe. Accusantium
+              eveniet nostrum quos in fugi Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Facere laudantium in debitis nisi
+              laboriosam possimus saepe. Accusantium eveniet nostrum quos in
+              fugiat, architecto impedit possimus dolores, minima non
+              perspiciatis. Recusandae!at, architecto impedit possimus dolores,
+              minima non perspiciatis. Recusandae!
+            </DetectScrollCardComponent>
+            <DetectScrollCardComponent>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+              laudantium in debitis nisi lab Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Facere laudantium in debitis nisi
+              laboriosam possimus saepe. Accusantium eveniet nostrum quos in
+              fugiat, architecto impedit possimus dolores, minima non
+              perspiciatis. Recusandae!oriosam possimus saepe. Accusantium
+              eveniet nostrum quos in fugiat, architecto impedit possimus
+              dolores, minima non perspiciatis. Recusandae!
+            </DetectScrollCardComponent>
+            <DetectScrollCardComponent>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+              laudantium in debitis nis Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Facere laudantium in debitis nisi laboriosam
+              possimus saepe. Accusantium eveniet nostrum quos in fugiat,
+              architecto impedit possimus dolores, minima non perspiciatis.
+              Recusandae!i laboriosam possimus saepe. Accusantium eveniet
+              nostrum quos in fugiat, architecto impedit possimus dolores,
+              minima non perspiciatis. Recusandae!
+            </DetectScrollCardComponent>
+            <DetectScrollCardComponent>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+              laudantium in debitis nisi laboriosam possimus saepe. Accusantium
+              eveniet nostrum quos in Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Facere laudantium in debitis nisi laboriosam
+              possimus saepe. Accusantium eveniet nostrum quos in fugiat,
+              architecto impedit possimus dolores, minima non perspiciatis.
+              Recusandae! fugiat, architecto impedit possimus dolores, minima
+              non perspiciatis. Recusandae!
+            </DetectScrollCardComponent>
+            <DetectScrollCardComponent>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+              laudantium in debitis nisi laboriosam possimus saepe. Accusantium
+              eveniet nostrum quos in fugiat, architecto impedit possimus
+              dolores, minima non perspiciatis. Recusandae! Lorem ipsum dolor
+              sit amet, consectetur adipisicing elit. Facere laudantium in
+              debitis nisi laboriosam possimus saepe. Accusantium eveniet
+              nostrum quos in fugiat, architecto impedit possimus dolores,
+              minima non perspiciatis. Recusandae!
+            </DetectScrollCardComponent>
           </div>
         </div>
-      </div>
-    </section>
-    <LogoComponent />
-    <section ></section>
-  </Layout>
-);
+      </section>
+    </Layout>
+  );
+};
 
 export default IndexPage;
