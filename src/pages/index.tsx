15718f6ec76b021dfbe03282af26e9160d4c6bdd
sumi.byun <selina2000@naver.com>
2022-01-15 14:55:17 +0900

    Fix : scroll component 정리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,63 +3,39 @@ import React, { useState, useEffect, FunctionComponent, useRef } from 'react';
 import './index.scss';
 import Layout from 'components/layout';
 import debounce from 'lodash/debounce';
-
-const DetectScrollTextComponent: FunctionComponent = () => {
-  const scrollRef = useRef(null);
-  const delay = 15;
-  const [isShow, setIsShow] = useState(false);
-  useEffect(() => {
-    if (!scrollRef.current) return;
-    window.addEventListener('scroll', debounce(yScrollEvent, delay));
-    return () => {
-      window.removeEventListener('scroll', yScrollEvent);
-    };
-  }, [scrollRef.current]);
-  const yScrollEvent = () => {
-    const scroll: DOMRect = scrollRef.current.getBoundingClientRect();
-    if (scroll.top < window.innerHeight - 150) {
-      setIsShow(true);
-    } else {
-      setIsShow(false);
-    }
-  };
-  return (
-    <section className={`text ${isShow ? 'active' : ''}`} ref={scrollRef}>
-      <div className="text__inner">
-        <div className="text__main">PROBRAIN</div>
-        <div className="text__sub">professional brain group</div>
-      </div>
-    </section>
-  );
+type ScrollProps = {
+  classname: string;
+  scrollActiveLocation: number; //얼마나 보이고 이벤트 시작할지
 };
-const DetectScrollCardComponent: FunctionComponent = ({ children }) => {
+const ScrollComponent: FunctionComponent<ScrollProps> = ({
+  children,
+  classname,
+  scrollActiveLocation,
+}) => {
   const scrollRef = useRef(null);
   const delay = 15;
   const [isShow, setIsShow] = useState(false);
   useEffect(() => {
     if (!scrollRef.current) return;
     window.addEventListener('scroll', debounce(yScrollEvent, delay));
-    // window.addEventListener('scroll', yScrollEvent);
     return () => {
       window.removeEventListener('scroll', yScrollEvent);
     };
   }, [scrollRef.current]);
   const yScrollEvent = () => {
     const scroll: DOMRect = scrollRef.current.getBoundingClientRect();
-    if (scroll.top < window.innerHeight - 150) {
+    if (scroll.top < window.innerHeight - scrollActiveLocation) {
       setIsShow(true);
     } else {
       setIsShow(false);
     }
   };
-
   return (
-    <div className={`c1__card ${isShow ? 'active' : ''}`} ref={scrollRef}>
+    <div className={`${classname} ${isShow ? 'active' : ''}`} ref={scrollRef}>
       {children}
     </div>
   );
 };
-
 const IndexPage = () => {
   return (
     <Layout>
@@ -100,11 +76,16 @@ const IndexPage = () => {
           </div>
         </div>
       </section>
-      <DetectScrollTextComponent />
+      <ScrollComponent classname="text" scrollActiveLocation={150}>
+        <div className="text__inner">
+          <div className="text__main">PROBRAIN</div>
+          <div className="text__sub">professional brain group</div>
+        </div>
+      </ScrollComponent>
       <section className="c1">
         <div className="c1__inner">
           <div className="c1__content">
-            <DetectScrollCardComponent>
+            <ScrollComponent classname="c1__card" scrollActiveLocation={150}>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
               laudantium in debitis nisi laboriosam possimus saepe. Accusantium
               eveniet nostrum quos in fugi Lorem ipsum dolor sit amet,
@@ -113,47 +94,27 @@ const IndexPage = () => {
               fugiat, architecto impedit possimus dolores, minima non
               perspiciatis. Recusandae!at, architecto impedit possimus dolores,
               minima non perspiciatis. Recusandae!
-            </DetectScrollCardComponent>
-            <DetectScrollCardComponent>
+            </ScrollComponent>
+            <ScrollComponent classname="c1__card" scrollActiveLocation={150}>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-              laudantium in debitis nisi lab Lorem ipsum dolor sit amet,
+              laudantium in debitis nisi laboriosam possimus saepe. Accusantium
+              eveniet nostrum quos in fugi Lorem ipsum dolor sit amet,
               consectetur adipisicing elit. Facere laudantium in debitis nisi
               laboriosam possimus saepe. Accusantium eveniet nostrum quos in
               fugiat, architecto impedit possimus dolores, minima non
-              perspiciatis. Recusandae!oriosam possimus saepe. Accusantium
-              eveniet nostrum quos in fugiat, architecto impedit possimus
-              dolores, minima non perspiciatis. Recusandae!
-            </DetectScrollCardComponent>
-            <DetectScrollCardComponent>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-              laudantium in debitis nis Lorem ipsum dolor sit amet, consectetur
-              adipisicing elit. Facere laudantium in debitis nisi laboriosam
-              possimus saepe. Accusantium eveniet nostrum quos in fugiat,
-              architecto impedit possimus dolores, minima non perspiciatis.
-              Recusandae!i laboriosam possimus saepe. Accusantium eveniet
-              nostrum quos in fugiat, architecto impedit possimus dolores,
+              perspiciatis. Recusandae!at, architecto impedit possimus dolores,
               minima non perspiciatis. Recusandae!
-            </DetectScrollCardComponent>
-            <DetectScrollCardComponent>
+            </ScrollComponent>
+            <ScrollComponent classname="c1__card" scrollActiveLocation={150}>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
               laudantium in debitis nisi laboriosam possimus saepe. Accusantium
-              eveniet nostrum quos in Lorem ipsum dolor sit amet, consectetur
-              adipisicing elit. Facere laudantium in debitis nisi laboriosam
-              possimus saepe. Accusantium eveniet nostrum quos in fugiat,
-              architecto impedit possimus dolores, minima non perspiciatis.
-              Recusandae! fugiat, architecto impedit possimus dolores, minima
-              non perspiciatis. Recusandae!
-            </DetectScrollCardComponent>
-            <DetectScrollCardComponent>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-              laudantium in debitis nisi laboriosam possimus saepe. Accusantium
-              eveniet nostrum quos in fugiat, architecto impedit possimus
-              dolores, minima non perspiciatis. Recusandae! Lorem ipsum dolor
-              sit amet, consectetur adipisicing elit. Facere laudantium in
-              debitis nisi laboriosam possimus saepe. Accusantium eveniet
-              nostrum quos in fugiat, architecto impedit possimus dolores,
+              eveniet nostrum quos in fugi Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Facere laudantium in debitis nisi
+              laboriosam possimus saepe. Accusantium eveniet nostrum quos in
+              fugiat, architecto impedit possimus dolores, minima non
+              perspiciatis. Recusandae!at, architecto impedit possimus dolores,
               minima non perspiciatis. Recusandae!
-            </DetectScrollCardComponent>
+            </ScrollComponent>
           </div>
         </div>
       </section>
