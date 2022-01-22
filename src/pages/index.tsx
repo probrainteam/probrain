ef79df5720f2e44116b9d14c112fe5f7af7410dd
sumi.byun <selina2000@naver.com>
2022-01-22 16:12:35 +0900

    Feat : Add Welcome section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,24 @@ import ScrollComponent from 'components/molecules/Scroll';
 const IndexPage = () => {
   return (
     <Layout>
+      <section className="welcome">
+        <div className="welcome__inner">
+          <div className="welcome__top">
+            <div className="welcome__title">
+              <div className="welcome__title--light">PROBRAIN과</div>
+              <div className="welcome__title--light">함께 추억을 쌓을</div>
+              <div className="welcome__title--bold">
+                멋진 동료를 찾고 있어요
+              </div>
+            </div>
+            <button className="btn btn--grey">PROBRAIN 가입 신청</button>
+          </div>
+          <div className="welcome__image">
+            {/* picture? */}
+            {/* <img src={require('/images/mainImage.png').default} alt="" /> */}
+          </div>
+        </div>
+      </section>
       {/* <section className="intro">
         <div className="intro__inner">
           <div className="intro__title">
