2c3e2054064b85cfa8253d5768734aeb8f2fb66e
sumi.byun <selina2000@naver.com>
2022-01-22 17:32:44 +0900

    Feat : Add Tmi section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ const IndexPage = () => {
           </div>
         </div>
       </section>
-      <div className="activity">
+      <section className="activity">
         <div className="activity__inner">
           <div className="activity__title">
             <div className="activity__title--main">ACTIVITY</div>
@@ -111,7 +111,95 @@ const IndexPage = () => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="tmi">
+        <div className="tmi__inner">
+          <div className="tmi__title">
+            <div className="tmi__title--main">PROBRAIN TMI</div>
+            <div className="tmi__title--sub">PROBRAIN 의 사소한 정보들!</div>
+          </div>
+          <div className="tmi__content">
+            <div className="card">
+              <div className="card__text">
+                <div className="card__title">
+                  01 전통이 있는 동아리 (since 2002)
+                </div>
+                <div className="card__detail">
+                  개발이라는 같은 관심사를 가지고, 성장이라는 같은 목적을 가진
+                  사람들이 모이면 개발에 대한 이런저런 이야기를 나누는 것은 전혀
+                  이상한 일이 아니죠. 오직 개발 관련 정보를 공유하기 위해 개설된
+                  카카오톡 오픈 채팅방과 디스코드 채널에서 눈치보지 않고
+                  자유롭게 이야기를 나눠보세요.
+                </div>
+                <div className="card__detail--sub">
+                  <ul>
+                    <li>카카오톡 오픈 채팅방 운영</li>
+                    <li>전체 & 팀별 디스코드 운영</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card__text">
+                <div className="card__title">02 의욕적인 동아리</div>
+                <div className="card__detail">
+                  내가 생각하는 것을 다른 사람들은 어떻게 생각할까요? 하나의
+                  공통된 주제에 대해서 이야기를 나눌 수 있는 정기적인 세미나를
+                  개최해요. 월 별로 정해지는 주제에 대해서 개인적으로 공부하고
+                  준비해서 발표하는 시간을 가져봅시다.
+                </div>
+                <div className="card__detail--sub">
+                  <ul>
+                    <li>앱/ 프론트엔드 / 백엔드로 나뉘어 개인 발표</li>
+                    <li>월 별로 진행</li>
+                    <li>회원들이 직접 돌아가며 주제 선정</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card__text">
+                <div className="card__title">
+                  03 개발과 친목이 공존하는 동아리
+                </div>
+                <div className="card__detail">
+                  혼자 공부하다보면 지치고 힘들 때가 많은 것 같아요. 팀원들과
+                  같이 모여서 각자 코딩하는 모각코를 진행합니다. 분명 개발에
+                  생기를 불어넣어주는 활동이 될거에요.
+                </div>
+                <div className="card__detail--sub">
+                  <ul>
+                    <li>온라인 / 오프라인 다양한 형태 선정</li>
+                    <li>자유로운 형식과 계획 작성</li>
+                    <li>PROB junior 회원들의 기술블로그 링크 게시</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card__text">
+                <div className="card__title">
+                  04 선배들의 OUTPUT이 좋은 동아리
+                </div>
+                <div className="card__detail">
+                  포지션과 수준에 맞춰 팀이 꾸려져요. 팀 단위로 프로젝트를
+                  진행하고 학기 말에 발표회를 진행해요. 팀 별로 만든 프로젝트를
+                  테스트 및 배포하고 기회가 된다면 다양한 대회에 출전도
+                  해보세요.
+                </div>
+                <div className="card__detail--sub">
+                  <ul>
+                    <li>
+                      간단한 토이 프로젝트부터 정식 서비스를 위한 프로젝트까지
+                    </li>
+                    <li>외부 디자이너 팀원 영입 가능</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       {/* <section className="intro">
         <div className="intro__inner">
           <div className="intro__title">
