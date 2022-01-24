ba279d809929d3b047326ed5e9430e101e494130
jeonbyeongmin <qudals7613@gmail.com>
2022-01-24 23:12:26 +0900

    add : dark-mode (css custom property)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from 'react';
 
-// You can delete this file if you're not using it
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    React.createElement('script', {
+      key: 0,
+      dangerouslySetInnerHTML: {
+        __html: `
+        (() => {
+
+          let preferredTheme;
+          try {
+            preferredTheme = localStorage.getItem('color-mode');
+          } catch (err) { }
+
+          const setTheme = (newTheme) => {
+            window.__theme = newTheme;
+            preferredTheme = newTheme;
+            document.body.className = newTheme;
+          }
+
+          window.__setPreferredTheme = (newTheme) => {
+            setTheme(newTheme);
+            try {
+              localStorage.setItem('color-mode', newTheme);
+            } catch (err) {}
+          }
+
+          let darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+          darkQuery.addListener(function (e) {
+            window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+          })
+
+          setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
+        })();
+        `,
+      },
+    }),
+  ]);
+};
--- src/components/molecules/nav/index.tsx
+++ src/components/molecules/nav/index.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './nav.scss';
 import { Link } from 'gatsby';
 import Text from 'components/atoms/text';
+import { MoonIcon } from 'components/atoms/icon';
+
+declare global {
+  interface Window {
+    __theme: string;
+    __setPreferredTheme(colorMode: string): void;
+  }
+}
 
 const Nav = () => {
+  const getInitialColorMode = () => {
+    if (typeof window !== 'undefined') {
+      return window.__theme;
+    } else {
+      return 'light';
+    }
+  };
+
+  const [colorMode, setColorMode] = useState(getInitialColorMode());
+
+  const darkModeHandling = () => {
+    setColorMode(colorMode === 'dark' ? 'light' : 'dark');
+  };
+
+  useEffect(() => {
+    window.__setPreferredTheme(colorMode);
+  }, [colorMode]);
+
   return (
     <nav className="nav">
       <div className="nav__inner">
@@ -16,6 +42,9 @@ const Nav = () => {
         <Link to="/contact" className="nav__item">
           <Text className="nav">CONTACT</Text>
         </Link>
+        <div className="nav__icon-wrapper" onClick={darkModeHandling}>
+          <MoonIcon />
+        </div>
       </div>
     </nav>
   );
